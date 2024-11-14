b301017cc189a8656696c2f401ea8bcc4c0ee041
sthayuzal <97941356+sthayuzal@users.noreply.github.com>
2024-11-14 09:43:34 +0000

    conversionoperator

---
+++ .github/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553  //value that can't be changed
+let accountEmail = "ujjwalshrestha2058@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kathmandu"
+
+//accountId =2 //not allowed
+
+accountEmail = "ujjwalstha@gmail.com"
+accountCity = "Hetauda"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
