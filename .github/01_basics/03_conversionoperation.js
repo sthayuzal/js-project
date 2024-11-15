3bbdb2b8115fce73a489a88ef5d84f62a0307f78
sthayuzal <97941356+sthayuzal@users.noreply.github.com>
2024-11-15 04:53:22 +0000

    operations added

--- .github/01_basics/03_conversionoperation.js
+++ .github/01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1 
  
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 // "" => false
@@ -23,7 +23,25 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(someNumber);
+// console.log(stringNumber);
+// console.log(someNumber);
 
+//****************operations******************************* 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%2);
+
+let str ="hello"
+let str1 = "Yuzal"
+
+let str2 =  str+str1
+
+console.log(str2);
 
