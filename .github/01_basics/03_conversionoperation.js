51a73135400f58260924c230cacf180c3dab402a
Sthayuzal <ujjwal.076bei028@acem.edu.np>
2024-11-25 12:18:57 +0545

    first commit

---
+++ .github/01_basics/03_conversionoperation.js
@@ -0,0 +1,47 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1 
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(someNumber);
+
+//****************operations******************************* 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%2);
+
+let str ="hello"
+let str1 = "Yuzal"
+
+let str2 =  str+str1
+
+console.log(str2);
+
